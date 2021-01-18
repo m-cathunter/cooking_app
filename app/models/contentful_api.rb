c2a30766cc547369c55c393fe1d9175f8fa5ced7
Magdalena Kotołowska <m.kotolowska@gmail.com>
2021-01-18 09:59:49 +0100

    Add method to create contentful client

--- app/models/contentful_api.rb
+++ app/models/contentful_api.rb
@@ -1,2 +1,11 @@
 class ContentfulApi
+  # Create contentful client
+  def self.client
+    @client ||= Contentful::Client.new(
+      access_token: ENV['ACCESS_TOKEN'],
+      space: ENV['SPACE'],
+      dynamic_entries: :auto,
+      raise_errors: true
+    )
+  end
 end
