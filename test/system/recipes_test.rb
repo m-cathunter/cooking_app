dadeb7be875579076df924abd7daa1535f716ae0
Magdalena Kotołowska <m.kotolowska@gmail.com>
2021-01-18 16:13:03 +0100

    Create test checking information about a recipe
    on the show page

--- test/system/recipes_test.rb
+++ test/system/recipes_test.rb
@@ -5,9 +5,16 @@ class RecipesTest < ActionDispatch::SystemTestCase
     @recipe = ContentfulApi.recipes.first
   end
 
-  test "check information about a recipe" do
+  test "check information about a recipe on index page" do
     visit root_path
     assert_text @recipe.fields[:title]
     assert_selector(:xpath, "//img[@src='#{@recipe.photo.url}']")
+  end
+
+  test "check information about a recipe on show page" do
+    visit recipe_path(@recipe.id)
+    assert_text @recipe.fields[:title]
+    assert_selector(:xpath, "//img[@src='#{@recipe.photo.url}']")
+    assert_text @recipe.fields[:description]
   end
 end
